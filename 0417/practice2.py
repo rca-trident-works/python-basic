db15b03341836c4a268161bd3e71ad85099a7e82
rca <nem@nem.x0.to>
2025-04-17 16:22:59 +0900

    practice2

--- 0417/practice2.py
+++ 0417/practice2.py
@@ -1,9 +1,6 @@
 s = "Python is simple and easy."
 
-# Make array ['Simple', ' python ', 'PANDA', 'e-a-s-y']
-
 splitted = s.split()
-
 ans = []
 
 # 1
@@ -15,5 +12,9 @@ ans.append(" " + splitted[0].lower() + " ")
 # 3
 ans.append((splitted[0][0] + splitted[3][0:] + splitted[3][0]).upper())
 
+# 4
+ans.append("-".join(splitted[4][:-1]))
 
+
+# Print the result
 print(ans)
