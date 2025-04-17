0f73601ab2523bae0eac7cb5f54616cf67feb5b4
rca <nem@nem.x0.to>
2025-04-17 16:38:05 +0900

    2-5

---
+++ 0417/practice2-5.py
@@ -0,0 +1,13 @@
+num = "123.45"
+
+# 1. Convert string to float
+num = float(num)
+
+# 2. Convert float to integer (truncating the decimal part)
+num = int(num)
+
+# 3. Convert integer to string
+num = str(num)
+
+# (debug) Print the message if the conversion is successful
+print('Reached End of the program without errors!')
