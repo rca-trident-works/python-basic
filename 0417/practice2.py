745afa7c1a184d3192ca54b3ba572aa4e2729291
rca <nem@nem.x0.to>
2025-04-17 16:20:07 +0900

    remove helper comment for copilot

--- 0417/practice2.py
+++ 0417/practice2.py
@@ -12,7 +12,7 @@ ans.append(splitted[2].capitalize())
 # 2
 ans.append(" " + splitted[0].lower() + " ")
 
-# 3 (Pick target char from string)
+# 3
 ans.append((splitted[0][0] + splitted[3][0:] + splitted[3][0]).upper())
 
 
