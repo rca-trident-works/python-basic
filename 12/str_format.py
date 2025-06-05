366c1e46287559b899a8c09af54206e88a387471
rca <_@r0m.me>
2025-06-05 13:54:24 +0900

    add str_format

---
+++ 12/str_format.py
@@ -0,0 +1,15 @@
+# format method
+print('x={}, y={}'.format(10, 20))
+print('x={0}, y={0}, z={1}'.format(10, 20))
+
+s = 'a = {0}, b = {1}, c = {2}'.format(100, 200, 300)
+print(s)
+
+# f-string python 3.6以降で利用可
+x = 10
+y = 20
+print(f'x={x}, y={y}')
+print(f'x={x}, y={x}, z={y}')
+
+s = f'a = {100}, b = {200}, c = {300}'
+print(s)
