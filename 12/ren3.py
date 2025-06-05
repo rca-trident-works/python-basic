91afede475c71ac8598866ebdcb809572845dc37
rca <_@r0m.me>
2025-06-05 14:20:59 +0900

    add ren3

---
+++ 12/ren3.py
@@ -0,0 +1,26 @@
+import os
+
+# Static
+path = "./word.txt"
+
+def load_words():
+    # ファイルが存在するなら読み込む
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as file:
+            words = file.read().splitlines()
+        return words
+    else:
+        return []
+
+
+def main():
+    words = load_words()
+
+    count = 0
+
+    for word in words:
+        count += 1
+        print(f"{count:04}:{word}")
+
+if __name__ == "__main__":
+    main()
