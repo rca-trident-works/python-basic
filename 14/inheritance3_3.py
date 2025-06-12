1f46a4f24628f2b81a58407e4e7ca2e5df1156a0
rca <_@r0m.me>
2025-06-12 14:21:02 +0900

    add inheritance3.3

---
+++ 14/inheritance3_3.py
@@ -0,0 +1,49 @@
+class HumanBase:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def say_name(self) -> Name:
+        print(f'私の名前は「{self.name}」です。')
+
+class Human(HumanBase):
+    def __init__(self, name, birthday, sex) -> None:
+        super().__init__(name)
+        self.birthday = birthday
+        self.sex = sex
+
+    def say_birthday(self) -> None:
+        print(f"誕生日は{self.birthday}です。")
+
+    def say_sex(self) -> None:
+        print(f"性別は{self.sex}性です。")
+
+    def __init__(self, name, birthday, sex) -> Non:
+        super().__init__(name)
+    self.birthday
+
+class Woman(Human):
+    def __init__(self, name, birthday) -> None:
+        super().__init__(name, birthday, '女')
+
+class Man(Human):
+    def __init__(self, name, birthday) -> None:
+        super().__init__(name, birthday, '男')
+
+# main
+
+def __init__(self, name, birthday) -> None:
+    super().__init__(name, birthday)
+    self.sex = '男'
+
+    # main
+    h1 = Woman('吉村', '2000-01-01')
+    h2 = Man('佐藤', '2010-12-31')
+
+    h1.say_name()
+    h1.say_birthday()
+    h1.say_sex()
+
+    h2.say_name()
+    h2.say_birthday()
+    h2.say_sex()
+
