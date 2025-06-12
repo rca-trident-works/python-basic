5743bab08509110481d877bc9d951d5a4867ecdc
rca <_@r0m.me>
2025-06-12 16:18:08 +0900

    fix??????????

--- 14/inheritance3_3.py
+++ 14/inheritance3_3.py
@@ -2,7 +2,7 @@ class HumanBase:
     def __init__(self, name) -> None:
         self.name = name
 
-    def say_name(self) -> Name:
+    def say_name(self) -> None:
         print(f'私の名前は「{self.name}」です。')
 
 class Human(HumanBase):
@@ -25,21 +25,16 @@ class Man(Human):
     def __init__(self, name, birthday) -> None:
         super().__init__(name, birthday, '男')
 
+
 # main
+h1 = Woman('吉村', '2000-01-01')
+h2 = Man('佐藤', '2010-12-31')
 
-def __init__(self, name, birthday) -> None:
-    super().__init__(name, birthday)
-    self.sex = '男'
+h1.say_name()
+h1.say_birthday()
+h1.say_sex()
 
-    # main
-    h1 = Woman('吉村', '2000-01-01')
-    h2 = Man('佐藤', '2010-12-31')
-
-    h1.say_name()
-    h1.say_birthday()
-    h1.say_sex()
-
-    h2.say_name()
-    h2.say_birthday()
-    h2.say_sex()
+h2.say_name()
+h2.say_birthday()
+h2.say_sex()
 
