093711a45aebbed330e7e093143ecc2d8be0ffd0
rca <_@r0m.me>
2025-06-13 15:03:40 +0900

    fix test

--- 15/test_class_person4.py
+++ 15/test_class_person4.py
@@ -7,9 +7,11 @@ from class_person4 import Customer
         (1, "株式会社テスト", "太郎", "山田", "氏名: 山田太郎\n\tid:1 会社名:株式会社テスト"),
     ]
 )
-def test_full_name(company_id, company_name, first_name, last_name, expected):
+def test_full_name(capsys, company_id, company_name, first_name, last_name, expected):
     c = Customer(company_id = company_id, 
                  company_name = company_name,
                  first_name = first_name,
                  last_name = last_name)
-    assert c.show() == expected
+    c.show()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected
