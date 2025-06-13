ea4c0c10bc0d647273f362729bb83f3e05e41d05
rca <_@r0m.me>
2025-06-13 14:45:02 +0900

    add test

---
+++ 15/test_class_person2.py
@@ -0,0 +1,20 @@
+import pytest
+from class_person2 import Customer
+
+@pytest.mark.parametrize(
+    ("company_id", "company_name", "first_name", "last_name", "expected"),
+    [
+        (None, None, None, None, "名無しの権兵衛"),
+        (1, None, "太郎", None, "名無しの太郎"),
+        (2, "株式会社テスト", "太郎", "山田", "山田太郎"),
+    ]
+)
+def test_full_name(company_id, company_name, first_name, last_name, expected):
+    c = Customer(company_id, company_name, first_name, last_name)
+    assert c.full_name == expected
+    # なんの意味もないけど模範解答がこのテストをしてるので
+    if company_id is not None:
+        assert c.company_id == company_id
+    if company_name is not None:
+        assert c.company_name == company_name
+
