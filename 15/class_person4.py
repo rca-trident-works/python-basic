1a73ec6b4e8ab7cc884f41a23790367e0b4a24e1
rca <_@r0m.me>
2025-06-13 14:56:29 +0900

    impl class_person4.py

---
+++ 15/class_person4.py
@@ -0,0 +1,10 @@
+from class_person3 import Person
+
+class Customer(Person):
+    def __init__(self, company_id=None, company_name=None, first_name=None, last_name=None):
+        super().__init__(first_name, last_name)
+        self.company_id = company_id
+        self.company_name = company_name
+
+    def show(self):
+        return f"{super().show()}\n\tid:{self.company_id} 会社名:{self.company_name}"
