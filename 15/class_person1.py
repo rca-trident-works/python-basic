da7de62ad810a03772193d8d0039e1781f0e0480
rca <_@r0m.me>
2025-06-13 14:35:51 +0900

    impl class_person1.py

---
+++ 15/class_person1.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, first_name = None, last_name = None):
+        if last_name is None:
+            self.first_name = '権兵衛' if first_name is None else first_name
+            self.last_name = '名無しの'
+        else:
+            self.first_name = first_name
+            self.last_name = last_name
+
+    @property
+    def full_name(self):
+        return f"{self.last_name}{self.first_name}"
+
