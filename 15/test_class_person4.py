c4931f19509f60fafb6fbf755d6dfdead9f0e320
rca <_@r0m.me>
2025-06-13 14:56:38 +0900

    add test

---
+++ 15/test_class_person4.py
@@ -0,0 +1,15 @@
+import pytest
+from class_person4 import Customer
+
+@pytest.mark.parametrize(
+    ("company_id", "company_name", "first_name", "last_name", "expected"),
+    [
+        (1, "株式会社テスト", "太郎", "山田", "氏名: 山田太郎\n\tid:1 会社名:株式会社テスト"),
+    ]
+)
+def test_full_name(company_id, company_name, first_name, last_name, expected):
+    c = Customer(company_id = company_id, 
+                 company_name = company_name,
+                 first_name = first_name,
+                 last_name = last_name)
+    assert c.show() == expected
