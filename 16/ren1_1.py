dc3f0fba7358ca3b3e47f5a271a7386096795ff4
rca <_@r0m.me>
2025-06-19 14:17:25 +0900

    add ren1_1.py

---
+++ 16/ren1_1.py
@@ -0,0 +1,10 @@
+import os
+
+def main():
+    current_directory = os.getcwd()
+    
+    # Print the current working directory
+    print(f"現在の作業ディレクトリは: {current_directory}")
+
+if __name__ == "__main__":
+    main()
