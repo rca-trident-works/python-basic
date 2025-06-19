5f57f4b6ec8f01e928620cfbe311f9815a30a99d
rca <_@r0m.me>
2025-06-19 16:31:02 +0900

    ? wip

--- 16/wc.py
+++ 16/wc.py
@@ -41,15 +41,23 @@ def print_results(file_paths, lines_results, words_results, bytes_results):
     if item_count > 1:
         for array in (lines_results, words_results, bytes_results):
             array.append(sum(array))
-
         file_paths.append("total")
-
         # Adjust item count to include the total row
         item_count += 1
 
-    lines_width = get_max_width(lines_results) + 1 # なぜか最初の列は4文字に繰り上げないっぽい？
-    words_width = max(get_max_width(words_results), 4)
-    bytes_width = max(get_max_width(bytes_results), 4)
+    lines_width = get_max_width(lines_results)
+    words_width = get_max_width(words_results)
+    bytes_width = get_max_width(bytes_results)
+
+    if (lines_width < 4 and words_width < 4 and bytes_width < 4):
+        lines_width += 1
+        # 全部が3桁未満の場合は3桁に揃えられるっぽい？
+        words_width = 3
+        bytes_width = 3
+    else:
+        lines_width += 1
+        words_width = max(words_width, 4) + 1
+        bytes_width = max(bytes_width, 4) + 1
 
     for i in range(item_count):
         print(f"{lines_results[i]:>{lines_width}} {words_results[i]:>{words_width}} {bytes_results[i]:>{bytes_width}} {file_paths[i]}")
